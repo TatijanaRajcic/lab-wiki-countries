377122a8d43817fe40f819eed5352defcfeb53fa
Tatijana Rajcic <tatijana.rajcic@essec.edu>
2019-08-08 15:50:38 +0200

    changed CountriesList component from function to class

--- src/components/CountriesList.js
+++ src/components/CountriesList.js
@@ -1,34 +1,43 @@
-import React from 'react';
+import React, {Component} from 'react';
 import CountryLine from "../components/CountryLine.js"
 import countries from "../countries.json";
 import {NavLink} from "react-router-dom";
 import "../stylesheets/CountriesList.css"
 
-function CountriesList() {
+class CountriesList extends Component {
 
-  let allCountries = countries;
+  constructor(props) {
+    super(props)
+    this.state = {
+      allCountries: countries
+    }
+  }
 
-  let wikiCountries = allCountries.map((country) => {
+  render() {
+
+    let wikiCountries = this.state.allCountries.map((country) => {
+      return (
+        <div>
+          <NavLink className="NavLink" activeClassName="ActiveNavLink" to={`/details/${country.cca3}`} >
+            <CountryLine 
+              id={country.cca3} 
+              name={country.name.common}
+              flag={country.flag}
+            />
+          </NavLink>
+        </div>
+      )
+    })
+  
     return (
-      <div>
-        <NavLink className="NavLink" activeClassName="ActiveNavLink" to={`/details/${country.cca3}`} >
-          <CountryLine 
-            id={country.cca3} 
-            name={country.name.common}
-            flag={country.flag}
-          />
-        </NavLink>
+      <div class="CountriesList">
+        <ul>
+          {wikiCountries}
+        </ul>
       </div>
     )
-  })
-
-  return (
-    <div class="CountriesList">
-      <ul>
-        {wikiCountries}
-      </ul>
-    </div>
-  )
+  }
+  
 }
 
 export default CountriesList;
